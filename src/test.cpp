5648573959dfed74ee866a921e97f8ee1f9cc896
Taylor Ellington <taylorellington101@gmail.com>
2018-12-13 00:41:53 -0800

    got test framework going

---
+++ src/test.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+
+namespace {
+TEST(zero, zero_test) { ASSERT_EQ(0, 0); }
+} // namespace
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
