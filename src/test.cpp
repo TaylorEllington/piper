c98d24e3908339bff169b04d100bfef1f996ad25
Taylor Ellington <taylorellington101@gmail.com>
2018-12-13 00:56:35 -0800

    fixed file org, added .clangformat file

--- src/test.cpp
+++ src/test.cpp
@@ -5,6 +5,6 @@ TEST(zero, zero_test) { ASSERT_EQ(0, 0); }
 } // namespace
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
